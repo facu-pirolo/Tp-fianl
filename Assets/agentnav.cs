038217e86edebb22f86ade0744c3a9dc9982808f
facu-pirolo <49518308@est.ort.edu.ar>
2025-11-27 16:53:05 -0300

    ray cast funciona

--- Assets/agentnav.cs
+++ Assets/agentnav.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 
 public class agentnav : MonoBehaviour
@@ -10,23 +11,48 @@ public class agentnav : MonoBehaviour
     [SerializeField] Transform targetTR;
     [SerializeField] Animator anim;
     [SerializeField] float velocity;
-
+    [SerializeField] Transform[] Puntos;
+    public bool patrullando = true;
+    private int currentPatrolPoint = 0;
+    public GameObject jugador;
+    public bool jugadorEnVision = false;
+    public int tiempoSinVision = 0;
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
-
     }
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
+        if (patrullando)
+        {
+            if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            {
+                currentPatrolPoint = (currentPatrolPoint + 1) % Puntos.Length;
+                agent.SetDestination(Puntos[currentPatrolPoint].position);
+            }
+        }
+        else
+        {
+            agent.SetDestination(jugador.transform.position);
+        }
         velocity = agent.velocity.magnitude;
         anim.SetFloat("Speed", velocity);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Perdiste");
+        }
     }
 }
--- Assets/raycast.cs
+++ Assets/raycast.cs
@@ -1,89 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
-using UnityEngine.SceneManagement;
-
 
 public class raycast : MonoBehaviour
 {
-    NavMeshAgent agent;
-    [SerializeField] Vector3 rayOrigin;
-    [SerializeField] Transform sightOrigin;
-    [SerializeField] float rayDistance = 10f;
-    [SerializeField] Transform[] puntosDePatru;
-    [SerializeField] raycast rayVision;
-    [SerializeField] bool Patrulaje = true;
-    int puntoActual = 0;
-    [SerializeField] Transform Jugador;
-    [SerializeField] Animator anim;
-    [SerializeField] float velocity;
-    float tiempoVision = 0f;
-    [SerializeField] float tiempoOlvido = 2f;
+    private agentnav agentScript;
+float RaycastDistancia = 5f;
+    public Rigidbody balaPrefab;
+    public Transform puntoDisparo;
+    public float fuerzaDisparo = 500f;
 
+    private float tiempoEntreDisparos = 1f;
+    private float contadorDisparo = 0f;
+    void Start()
+{
 
+    agentScript = GetComponent<agentnav>();
+}
 
-    // Start is called before the first frame update
-    void Awake()
+void Update()
+{
+    bool JugadorDetectado = false;
+
+    Debug.DrawRay(transform.position, transform.forward * RaycastDistancia, Color.red);
+
+    if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hitInfo, RaycastDistancia))
     {
-        agent = GetComponent<NavMeshAgent>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (!agent.pathPending && agent.remainingDistance < 0.3f && puntosDePatru.Length > 0)
+        if (hitInfo.collider.gameObject.tag == "Player")
         {
-            puntoActual = (puntoActual + 1) % puntosDePatru.Length;
-            agent.destination = puntosDePatru[puntoActual].position;
-        }
-        else
-        {
-            if (!Patrulaje)
-            {
-                agent.destination = Jugador.position;
-            }
-
-
-            tiempoVision += Time.deltaTime;
-            if (tiempoVision >= tiempoOlvido)
-            {
-                Patrulaje = true;
-                Jugador = null;
-                tiempoVision = 0f;
-            }
-        }
-        velocity = agent.velocity.magnitude;
-        anim.SetFloat("Speed", velocity);
-
-        Ray ray = new Ray(sightOrigin.position, sightOrigin.forward);
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit, rayDistance))
-        {
-            if (hit.collider.gameObject.tag == "Jugador")
-            {
-                Jugador = hit.collider.transform;
-                Debug.Log("Detectó a: " + hit.collider.gameObject.name);
-                Patrulaje = false;
-                tiempoVision = 0f;
-            }
-        }
-    }
-    public void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Jugador"))
-        {
-            SceneManager.LoadScene("Mensaje");
+                Disparar();
+                agentScript.patrullando = false;
+            agentScript.jugadorEnVision = true;
+            agentScript.tiempoSinVision = 0;
+            JugadorDetectado = true;
         }
     }
 
-
-
-    void OnDrawGizmos()
+    if (!JugadorDetectado)
     {
-        Color color = Color.red;
-        Gizmos.color = color;
-        Gizmos.DrawLine(sightOrigin.position, sightOrigin.position + sightOrigin.forward * rayDistance);
+        agentScript.jugadorEnVision = false;
     }
+
+    if (!agentScript.jugadorEnVision && !agentScript.patrullando)
+    {
+        agentScript.tiempoSinVision += (int)(Time.deltaTime * 100);
+        Debug.Log(agentScript.tiempoSinVision);
+        if (agentScript.tiempoSinVision >= 500)
+        {
+            agentScript.patrullando = true;
+            agentScript.tiempoSinVision = 0;
+        }
+    }
+}
+    void Disparar()
+    {
+        contadorDisparo += Time.deltaTime;
+
+        if (contadorDisparo < tiempoEntreDisparos) return;
+
+        contadorDisparo = 0f;
+
+        Rigidbody bala = Instantiate(balaPrefab, puntoDisparo.position, puntoDisparo.rotation);
+
+        bala.AddForce(puntoDisparo.forward * fuerzaDisparo);
+    }
+
 }
