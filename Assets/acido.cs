a39fcaa7e2a75e3c3d21e1eefdf6f7fa14706ac7
facu-pirolo <49518308@est.ort.edu.ar>
2025-11-27 17:17:24 -0300

    acido

---
+++ Assets/acido.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class acido : MonoBehaviour
+{
+    public float daño = 1f;
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Perdiste");
+        }
+
+        Destroy(gameObject);
+    }
+}
+
