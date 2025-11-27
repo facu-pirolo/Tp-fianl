4e90f04dc52fbfb8d862314dd835025820833029
facu-pirolo <49518308@est.ort.edu.ar>
2025-11-27 11:41:52 -0300

    .

---
+++ Assets/jugadoragarrarlanza.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jugadoragarrarlanza : MonoBehaviour
+{
+    public Transform weaponHolder;
+    public float pickupRange = 6f;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            TryPickup();
+        }
+    }
+
+    void TryPickup()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, pickupRange))
+        {
+            if (hit.collider.CompareTag("Weapon"))
+            {
+                hit.collider.GetComponent<agarrarlanza>().PullToHand(weaponHolder);
+            }
+        }
+    }
+}
